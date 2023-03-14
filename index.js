6e15a81570d4b43c1d0866ce8bf277f06fd9ca7e
Tim <togden1999@gmail.com>
2023-03-14 14:39:36 -0400

    Add hexagon image

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ const server = http.createServer(app);
 const { Server } = require('socket.io');
 const io = new Server(server);
 
+app.use('/static', express.static(__dirname + '/static'));
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
+
     socket.on('chat message', (msg) => {
         io.emit('chat message', msg);
         console.log('message: ' + msg);
     });
+
     socket.on('disconnect', () => {
         console.log('user disconnected');
     });
